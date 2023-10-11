98536219fb76a0f13057c9ff0fcfb6d5a0f13a32
JustAnotherPeople <29780715+ilhamfarabi@users.noreply.github.com>
2023-10-11 18:43:09 +0700

    Add files via upload

---
+++ DemoNilai.java
@@ -0,0 +1,8 @@
+package Tugas1_mahasiswa;
+
+public class DemoNilai {
+    public static void main(String[] args) {
+        nilai nilaiWong = new nilai();
+        nilaiWong.CetakNilai("82347384837", "Ilham Arif Farabi", 80, 85, 95, 90);
+    }
+}
